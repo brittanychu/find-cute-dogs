32c5e2d687309abd68709f18d98fe0f3bb518873
Brittany Chu <brittany.s.chu@gmail.com>
2018-02-27 17:11:06 -0500

    added secrets file

--- app/components/AllThePuppies.jsx
+++ app/components/AllThePuppies.jsx
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
-// import {
-//   Text,
-//   View,
-//   StyleSheet,
-//   Image,
-//   Dimensions,
-//   FlatList,
-// } from 'react-native';
 import { NavLink } from 'react-router-dom';
-
-const accessToken = "1328896.fc743b9.34ea0558e80a4696839efae1a9d19038";
+import Instafeed from 'react-instafeed';
+import {accessToken, clientId} from '../../secrets';
 
 export default class Home extends Component {
+  constructor(){
+    super();
+  }
+
   render() {
+    const instafeedTarget = 'instafeed';
     return (
       <div id="puppies">
         <title> <h1> Here are some puppies </h1> </title>
-        <puppies> </puppies>
+        <div id={instafeedTarget}>
+          <Instafeed
+            limit='5'
+            ref='instafeed'
+            resolution='standard_resolution'
+            sortBy='most-recent'
+            target={instafeedTarget}
+            template=''
+            tagName='puppies'
+            userId='userIdInstagramApiString'
+            clientId={`${clientId}`}
+            accessToken={`${accessToken}`}
+          />
+        </div>
       </div>
     )
   }
